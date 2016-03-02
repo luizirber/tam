66e0caec7dae25e1904ee688d3536690bf1acbf1
Luiz Irber <luiz.irber@gmail.com>
2016-03-02 14:26:39 -0800

    Remove pypy,2.7,3.3, fix classifiers

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ from os.path import dirname
 from os.path import join
 from os.path import splitext
 
-
 from setuptools import find_packages
 from setuptools import setup
 
@@ -42,20 +41,17 @@ setup(
     zip_safe=False,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 1 - Planning',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: Unix',
         'Operating System :: POSIX',
         'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
         # uncomment if you test on these interpreters:
         # 'Programming Language :: Python :: Implementation :: IronPython',
         # 'Programming Language :: Python :: Implementation :: Jython',
